427bfcc72fcd129eef53103e1b0329fbe5efef83
james <jfschmiechen@gmail.com>
2019-04-28 15:23:14 -0700

    Updates

---
+++ precache-manifest.f06b056544dbfbebeed62db3355313b5.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780c9826a89bd325ef14a35b209d7234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd65555278757d6ec066",
+    "url": "/static/css/2.a659536a.chunk.css"
+  },
+  {
+    "revision": "b475e06e7d674e77ff12",
+    "url": "/static/css/main.5d0a7b79.chunk.css"
+  },
+  {
+    "revision": "cd65555278757d6ec066",
+    "url": "/static/js/2.7948cfa3.chunk.js"
+  },
+  {
+    "revision": "b475e06e7d674e77ff12",
+    "url": "/static/js/main.53e0bead.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b2b06421dc85bd66885419e30eb2ba7",
+    "url": "/static/media/bc_channels.5b2b0642.png"
+  },
+  {
+    "revision": "c22b978e6f40eb937444b06e8354da4c",
+    "url": "/static/media/bc_dash.c22b978e.png"
+  },
+  {
+    "revision": "66ad1b56f14c0abed6e3d36b5e2d3d69",
+    "url": "/static/media/bc_settings.66ad1b56.png"
+  },
+  {
+    "revision": "e1fc5480306b4d22c955cda982879490",
+    "url": "/static/media/city.e1fc5480.jpg"
+  },
+  {
+    "revision": "2c5c5241e8774ef7c2b62f4f7f0f15c1",
+    "url": "/static/media/citysuperlowres.2c5c5241.jpg"
+  },
+  {
+    "revision": "0cd62bf74c4861c3f3060fab3cf8f6ef",
+    "url": "/static/media/edge_1.0cd62bf7.png"
+  },
+  {
+    "revision": "c0f879c5ae5a5ce0c624aea2ae1cf7ae",
+    "url": "/static/media/edge_a.c0f879c5.png"
+  },
+  {
+    "revision": "6da6c8ec10dedc5c26d4d6d298211704",
+    "url": "/static/media/edge_gradient.6da6c8ec.png"
+  },
+  {
+    "revision": "562a503ebfe77d31db3855cb673af114",
+    "url": "/static/media/formalMe.562a503e.jpg"
+  },
+  {
+    "revision": "e596c89f95c4eec822df04449c9da87a",
+    "url": "/static/media/jamesResume.e596c89f.pdf"
+  },
+  {
+    "revision": "03a123bc8be5b58f6f99ad21ef2b2a20",
+    "url": "/static/media/medical1.03a123bc.png"
+  },
+  {
+    "revision": "3310851b77fce842980643db12c94413",
+    "url": "/static/media/medical2.3310851b.png"
+  },
+  {
+    "revision": "e1b758f1425abb8ca3d9720499418cad",
+    "url": "/static/media/medical3.e1b758f1.png"
+  },
+  {
+    "revision": "b505bcf58469bf61d28a3b34f009e22a",
+    "url": "/static/media/preview_1.b505bcf5.png"
+  },
+  {
+    "revision": "8cc12dcd38e26203289c17a8e5376a8b",
+    "url": "/static/media/preview_a.8cc12dcd.png"
+  },
+  {
+    "revision": "341c668546e2e664fb1104b33796443a",
+    "url": "/static/media/preview_gradient.341c6685.png"
+  }
+]);
